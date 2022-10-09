f117d921e13eb1cc8d88eea172c4fa4cb278020f
Sangsoo Ahn <ahnbasic7@gmail.com>
2022-10-09 09:44:53 +0000

    finish cacheing

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
 from blog.forms import CommentForm
+from django.utils import timezone
+
+# from django.views.decorators.cache import cache_page
+
 import logging
 logger = logging.getLogger(__name__)
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.all()
-    logger.debug(f'Got {len(posts)} posts')
-    return render(request, "blog/index.html", {"posts":posts})
+# from django.views.decorators.vary import vary_on_cookie
 
+
+# Create your views here.
+# @cache_page(300)
+# def index(request):
+#     posts = Post.objects.all()
+#     logger.debug(f'Got {len(posts)} posts')
+#     return render(request, "blog/index.html", {"posts":posts})
+
+# @cache_page(300)
+# @vary_on_cookie
+def index(request):
+    # from django.http import HttpResponse
+    # return HttpResponse(str(request.user).encode("ascii"))
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
